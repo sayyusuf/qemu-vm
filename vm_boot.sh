a3dce1b232150391df08a1517919d97349eb918a
ysay <ysay@sayysf>
2023-09-11 17:11:55 +0300

    save

--- vm_boot.sh
+++ vm_boot.sh
@@ -1,5 +1,9 @@
-qemu-system-x86_64 -m 4G -smp 2 -enable-kvm \
-	-cdrom $1 \
-	-hda $2 \
-	-boot order=d
-
+if [[ -n $1 && -n $2 ]]
+then 
+	qemu-system-x86_64 -m 4G -smp 2 -enable-kvm \
+		-cdrom $1 \
+		-hda $2 \
+		-boot order=d
+else
+	echo "err: usage 'sudo ./vm_boot.sh isofile.iso  image.img'"
+fi
--- vm_up.sh
+++ vm_up.sh
@@ -1,5 +1,10 @@
-qemu-system-x86_64 -m 4G -smp 2 -enable-kvm \
-	-hda $1 \
-	-boot order=c \
-	-device e1000,netdev=net0 \
-	-netdev user,id=net0,hostfwd=tcp::5555-:22
+if [[ -n $1 && -n $2 && -n $3 ]]
+then 
+	qemu-system-x86_64 -m $1 -smp 2 -enable-kvm \
+		-hda $2 \
+		-boot order=c \
+		-device e1000,netdev=net0 \
+		-netdev user,id=net0,hostfwd=tcp::$3
+else
+	echo "err: usage 'sudo ./vm_up.sh 4G image.img 5555-:22'"
+fi
